bbc2bf89a154aec5b3d6dca7980766bd1e150992
Sean Oh <ohsean812@gmail.com>
2022-07-02 15:46:33 -0400

    commit tree 9a6c2059ee90c71622cf1d8595899badcc92deaf

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+
+// Test 1: Create a variable named "companyName" and
+// give it the value of "Scuber"
+// Test 2: Define it as a const
+const companyName = "Scuber";
+
+// Test 3: Create a variable named "mostProfitableNeighborhood" and
+// give it the value of "Chelsea"
+// Test 4: Define it as a let
+let mostProfitableNeighborhood = "Chelsea";
+
+// Test 5: Create a variable named "companyCeo" and
+// give it the value of "Susan Smith"
+// Test 6: Define it as a let
+let companyCeo = "Susan Smith";
